8453edb69ece1699b95344d589c8c868c03c2081
Landry Maillard <landrymaillard@gmail.com>
2024-03-18 16:43:11 +0100

    updated functions and broke buttons

--- assets/script/calls.js
+++ assets/script/calls.js
@@ -1,9 +1,4 @@
-addNewTaskTextarea()
-buttonCancel();
-displayNewTaskCreator();
 
-buttonDetailsTask("btn-category", "category");
-buttonDetailsTask("btn-deadline", "deadline");
-buttonDetailsTask("btn-priority", "priority");
-
-buttonAdd();
+// buttonDetailsTask("btn-category", "category");
+// buttonDetailsTask("btn-deadline", "deadline");
+// buttonDetailsTask("btn-priority", "priority");
--- assets/script/script.js
+++ assets/script/script.js
@@ -1,7 +1,20 @@
-function addNewTaskTextarea() {
+let inputTextarea = document.getElementById("input-new-task");
+let newTaskButton = document.querySelector(".btn-new-task");
+let newTaskCreator = document.querySelector(".new-task");
+newTaskCreator.hidden = true;
+let inputDetails = ["input-new-task", "category", "deadline", "priority"];
+let getDetails = {};
+let addButton = document.getElementById("btn-validate");
+let detailsClass = document.querySelectorAll(".input-details");
+
+
+function switchDisplay(element) {
+    element.hidden = !element.hidden
+}
+
+
     // Event listener for when the textarea is focused
-    var inputTextarea = document.getElementById("input-new-task");
-    inputTextarea.addEventListener("focus", function() {
+    inputTextarea.addEventListener("focus", (e) => {
         // Clear the textarea's value
         if (inputTextarea.value === "Add new task") {
             inputTextarea.value = "";
@@ -9,68 +22,59 @@ function addNewTaskTextarea() {
     });
 
     // Event listener for when the textarea loses focus
-    inputTextarea.addEventListener("blur", function() {
+    inputTextarea.addEventListener("blur", (e) => {
         // Restore the textarea's value if it's empty
         if (inputTextarea.value === "") {
             inputTextarea.value = "Add new task";
         }
     });
-}
 
-function buttonDetailsTask(buttonId, inputId) {
-    // Display of details buttons
-    document.getElementById(buttonId).addEventListener("click", function() {
-        event.preventDefault();
-        document.getElementById(inputId).style.display = "inline-block";
-        document.getElementById(buttonId).style.display = "none";
+
+// function buttonDetailsTask(buttonId, inputId) {
+//     // Display of details buttons
+//     document.getElementById(buttonId).addEventListener("click", (e) => {
+//         e.preventDefault();
+//         document.getElementById(inputId).hidden = false;
+//         document.getElementById(buttonId).hidden = true;
+//     });
+// }
+
+document.getElementById("btn-cancel").addEventListener("click", (e) => {
+    e.preventDefault();
+    // Erase textarea input
+    document.getElementById("input-new-task").value = "Add new task";
+    // Revert the buttons back to their original state
+    ["category", "deadline", "priority"].forEach(item => {
+        document.getElementById(item).style.display = "none";
+        document.getElementById("btn-" + item).style.display = "inline-block";
+        document.getElementById(item).value = "";
     });
-}
+    switchDisplay(newTaskCreator);
+    switchDisplay(newTaskButton);
+});
 
 
-function buttonCancel() {
-    document.getElementById("btn-cancel").addEventListener("click", function() {
-        event.preventDefault();
-        // Erase textarea input
-        document.getElementById("input-new-task").value = "Add new task";
-        // Revert the buttons back to their original state
-        ["category", "deadline", "priority"].forEach(item => {
-            document.getElementById(item).style.display = "none";
-            document.getElementById("btn-" + item).style.display = "inline-block";
-            document.getElementById(item).value = "";
-        });
-        document.querySelector(".new-task").style.display = "none";
-        document.querySelector(".btn-new-task").style.display = "block";
-    });
-}
 
-function displayNewTaskCreator() {
-    let newTaskButton = document.querySelector(".btn-new-task");
-    let newTaskCreator = document.querySelector(".new-task");
-    newTaskButton.addEventListener("click", function() {
-        event.preventDefault();
-        // Hide new task button and display creation menu
-        newTaskCreator.style.display = "block";
-        newTaskButton.style.display = "none";
-    });
-}
+newTaskButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    // Hide new task button and display creation menu
+    switchDisplay(newTaskCreator);
+    switchDisplay(newTaskButton);
+});
 
 function getDetailsValue() {
     // Get input values
-    let inputDetails = ["input-new-task", "category", "deadline", "priority"];
-    let getDetails = [];
     for (let i = 0; i < inputDetails.length; ++i) {
     getDetails[i] = document.getElementById(inputDetails[i]).value;
     }
-    console.log(getDetails);
+    console.log(getDetails); // remove it later
 }
 
-function buttonAdd() {
-    let addButton = document.getElementById("btn-validate");
-    addButton.addEventListener("click", function() {
-        event.preventDefault();
-        getDetailsValue();
+
+addButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    getDetailsValue();
     });
-}
 
 
 function newList() {
